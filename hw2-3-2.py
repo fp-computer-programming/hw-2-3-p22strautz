514a2e58658a2af7f179a62f617df8bb92f8469d
p22strautz <p22strautz@fairfieldprep.org>
2021-09-21 10:20:36 -0400

    hw2-3

---
+++ hw2-3-2.py
@@ -0,0 +1,10 @@
+# Author SCT (ADMG) 9/21/21
+
+radius = 4.6
+
+area = 3.14 * radius ** 2
+
+print(area)
+
+perimiter = 2 * 3.14 * radius
+print(perimiter)
---
+++ hw2-3-3.py
@@ -0,0 +1,13 @@
+# Author SCT (ADMG) 9/21/21
+
+width = 13.9
+
+height = 8.2
+
+area = width * height
+
+print(area)
+
+perimiter = (width * 2) + (height * 2)
+
+print(perimiter)
