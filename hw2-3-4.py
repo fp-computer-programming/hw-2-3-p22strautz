43919a61520c346140c4cce9d1a11ba5349c7801
p22strautz <p22strautz@fairfieldprep.org>
2021-09-23 10:16:38 -0400

    hw2-3

---
+++ hw2-3-4.py
@@ -0,0 +1,15 @@
+# Author SCT (ADMG) 9/23/21
+
+Population_start = 332774926
+
+rate_per_min = 60 / 25
+
+rate_per_hour = rate_per_min * 60
+
+rate_per_day = rate_per_hour * 24
+
+rate_per_year = rate_per_day * 365
+
+Population_End = rate_per_year + Population_start
+# Population_End is September 24th, 2022
+print(Population_End)
