4948a3de55022359815085a9000f7d8f05cc8adc
p22strautz <p22strautz@fairfieldprep.org>
2021-09-27 10:01:50 -0400

    hw2-3

--- hw2-3-4.py
+++ hw2-3-4.py
@@ -10,6 +10,6 @@ rate_per_day = rate_per_hour * 24
 
 rate_per_year = rate_per_day * 365
 
-Population_End = rate_per_year + Population_start
+Population_End = rate_per_year + Population_start + rate_per_day
 # Population_End is September 24th, 2022
 print(Population_End)
